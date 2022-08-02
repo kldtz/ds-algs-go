f2f4cfe98c0196737a010388fba721e4b6e6534c
Tobias Kolditz <tbs.kldtz@gmail.com>
2022-08-02 07:21:36 +0200

    Embed list in stack (without field names)

--- pkg/ds/stack.go
+++ pkg/ds/stack.go
@@ -1,33 +1,25 @@
 package ds
 
 type Stack[T any] struct {
-	list *List[T]
+	*List[T]
 }
 
 func NewStack[T any]() *Stack[T] {
-	return &Stack[T]{list: NewList[T]()}
+	return &Stack[T]{NewList[T]()}
 }
 
-func (stack *Stack[T]) push(v T) {
-	stack.list.Prepend(v)
+func (stack *Stack[T]) Push(v T) {
+	stack.Prepend(v)
 }
 
-func (stack *Stack[T]) peek() (T, bool) {
-	if stack.list.head == nil {
+func (stack *Stack[T]) Peek() (T, bool) {
+	if stack.head == nil {
 		var zero T
 		return zero, false
 	}
-	return stack.list.head.value, true
+	return stack.head.value, true
 }
 
-func (stack *Stack[T]) pop() (T, bool) {
-	return stack.list.RemoveFirst()
-}
-
-func (list *Stack[T]) Iterator() ListIterator[T] {
-	return list.Iterator()
-}
-
-func (stack *Stack[T]) Len() uint {
-	return stack.list.Len()
+func (stack *Stack[T]) Pop() (T, bool) {
+	return stack.RemoveFirst()
 }
--- pkg/ds/stack_test.go
+++ pkg/ds/stack_test.go
@@ -8,12 +8,12 @@ import (
 
 func TestPush(t *testing.T) {
 	stack := NewStack[int]()
-	stack.push(3)
-	stack.push(2)
-	stack.push(1)
+	stack.Push(3)
+	stack.Push(2)
+	stack.Push(1)
 
 	for i := 1; i < 4; i += 1 {
-		val, succ := stack.pop()
+		val, succ := stack.Pop()
 		assert.True(t, succ)
 		assert.Equal(t, i, val)
 	}
