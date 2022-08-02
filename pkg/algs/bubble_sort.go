2b01f1d3c0da4f42905e2a351d89db55507f8800
Tobias Kolditz <tbs.kldtz@gmail.com>
2022-08-02 20:15:30 +0200

    Update

--- pkg/algs/bubble_sort.go
+++ pkg/algs/bubble_sort.go
@@ -3,12 +3,14 @@ package algs
 import "golang.org/x/exp/constraints"
 
 func BubbleSort[T constraints.Ordered](xs []T) {
-	for n, last_swap := len(xs), 1; n > 1; n = last_swap {
+	for n := len(xs); n > 1; {
+		last_swap := 1
 		for i := 1; i < n; i += 1 {
 			if xs[i-1] > xs[i] {
 				Swap(xs, i-1, i)
 				last_swap = i
 			}
 		}
+		n = last_swap
 	}
 }
