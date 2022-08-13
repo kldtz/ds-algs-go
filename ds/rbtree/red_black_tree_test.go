1f1792d068e0dd805664b1958d570e23a4f9b858
Tobias Kolditz <tbs.kldtz@gmail.com>
2022-08-13 15:09:55 +0200

    Add deletion tests

--- ds/rbtree/red_black_tree_test.go
+++ ds/rbtree/red_black_tree_test.go
@@ -133,6 +133,14 @@ func TestFind(t *testing.T) {
 	assert.False(t, ok)
 }
 
+func TestHas(t *testing.T) {
+	tree := NewRBTree[int, int]()
+	tree.Insert(5, 5).Insert(2, 2).Insert(1, 1).Insert(3, 3)
+
+	ok := tree.Has(5)
+	assert.True(t, ok)
+}
+
 func TestSimpleDeletion(t *testing.T) {
 	tree := NewRBTree[int, string]()
 	tree.Insert(2, "").Insert(1, "").Insert(3, "")
@@ -144,4 +152,75 @@ func TestSimpleDeletion(t *testing.T) {
 	assert.Equal(t, tree.len, 2)
 	keys := Collect[int, string](tree.Levelorder(), getKey)
 	assert.Equal(t, []int{2, 1}, keys)
+}
+
+func TestDeletionUnknownElement(t *testing.T) {
+	tree := NewRBTree[int, string]()
+	tree.Insert(2, "").Insert(1, "").Insert(3, "")
+	assert.Equal(t, 3, tree.len)
+
+	ok := tree.Delete(4)
+	assert.False(t, ok)
+	assert.Equal(t, 3, tree.len)
+	keys := Collect[int, string](tree.Levelorder(), getKey)
+	assert.Equal(t, []int{2, 1, 3}, keys)
+}
+
+func TestDeletionCase1a2a(t *testing.T) {
+	tree := NewRBTree[int, string]()
+	for i := 1; i < 7; i += 1 {
+		tree.Insert(i, "")
+	}
+	assert.Equal(t, 6, tree.len)
+
+	tree.Delete(1)
+	assert.Equal(t, 5, tree.len)
+	keys := Collect[int, string](tree.Levelorder(), getKey)
+	assert.Equal(t, []int{4, 2, 5, 3, 6}, keys)
+}
+
+func TestDeletionCase3a4a(t *testing.T) {
+	tree := NewRBTree[int, string]()
+	tree.Insert(4, "").
+		Insert(1, "").
+		Insert(2, "").
+		Insert(3, "")
+
+	keys := Collect[int, string](tree.Levelorder(), getKey)
+	assert.Equal(t, []int{2, 1, 4, 3}, keys)
+
+	ok := tree.Delete(1)
+	keys = Collect[int, string](tree.Levelorder(), getKey)
+	assert.True(t, ok)
+	assert.Equal(t, []int{3, 2, 4}, keys)
+}
+
+func TestDeleteTwoKeys3a4a2b(t *testing.T) {
+	tree := NewRBTree[int, string]()
+	tree.Insert(4, "").
+		Insert(1, "").
+		Insert(2, "").
+		Insert(3, "")
+
+	tree.Delete(1)
+	tree.Delete(3)
+
+	keys := Collect[int, string](tree.Levelorder(), getKey)
+	assert.Equal(t, []int{4, 2}, keys)
+}
+
+func TestInorder(t *testing.T) {
+	tree := NewRBTree[int, string]()
+	tree.Insert(5, "5").Insert(2, "2").Insert(3, "3").Insert(1, "1").Insert(4, "4")
+
+	keys := Collect[int, string](tree.Inorder(), getKey)
+	assert.Equal(t, []int{1, 2, 3, 4, 5}, keys)
+
+	tree.Delete(5)
+	keys = Collect[int, string](tree.Inorder(), getKey)
+	assert.Equal(t, []int{1, 2, 3, 4}, keys)
+
+	tree.Delete(3)
+	keys = Collect[int, string](tree.Inorder(), getKey)
+	assert.Equal(t, []int{1, 2, 4}, keys)
 }
